acbd87eaaf820d368476e9b62e93e3bb83a248b0
Maria Iudina <iudina.maria@usm.md>
2025-03-14 10:30:57 +0200

    test.php

--- alternative_PHP/test.php
+++ alternative_PHP/test.php
@@ -1,9 +1,5 @@
 <?php
-/**
- * Подключаем файл для обработки теста.
- * В этом файле содержится логика обработки ответов пользователя и вычисления результатов.
- */
-require_once 'process_test.php'; // Подключаем логику из process_test.php
+require_once 'process_test.php';
 ?>
 
 <!DOCTYPE html>
@@ -11,55 +7,39 @@ require_once 'process_test.php'; // Подключаем логику из proce
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Прохождение теста</title>
+    <title>Вечный слава тебе, полупокер ты наш</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
-<h2>Пройти тест</h2>
+<h2>Начать кринж тайм...</h2>
 
-<!-- Форма для прохождения теста -->
 <form action="test.php" method="POST">
-    <label>Введите ваше имя:</label>
+    <label>Как обращаться? (оставишь пустое место - будешь полупокером)</label>
     <input type="text" name="username" required>
 
     <?php 
-    /**
-     * Перебираем все вопросы, загруженные из JSON.
-     * Каждый вопрос должен содержать текст и ответы, которые будут отображены пользователю.
-     */
     foreach ($questions as $index => $q): 
-        // Пропускаем вопросы, у которых нет текста или ответов
         if (empty($q["question"]) || empty($q["answers"])) continue;
     ?>
         <p><?php echo ($index + 1) . ". " .htmlspecialchars($q["question"]); ?></p>
         
         <?php 
-        /**
-         * Перебираем все ответы для текущего вопроса.
-         * В зависимости от типа ответа (radio или checkbox) создается соответствующий HTML-элемент.
-         */
         foreach ($q["answers"] as $key => $answer): 
-            $inputType = htmlspecialchars($q["type"]);  // Тип элемента (radio или checkbox)
-            $inputName = "answer[$index]"; // Имя для передачи данных в POST
+            $inputType = htmlspecialchars($q["type"]); 
+            $inputName = "answer[$index]";
 
-            /**
-             * Если тип ответа - checkbox, то добавляем [] в имя,
-             * чтобы можно было передавать массив выбранных значений.
-             */
             if ($inputType == 'checkbox') {
                 $inputName .= '[]';
             }
         ?>
-            <!-- Генерируем поле ввода для ответа -->
             <input type="<?= $inputType ?>" name="<?= $inputName ?>" value="<?= $key ?>"> 
             <?=htmlspecialchars($answer) ?><br>
         <?php endforeach; ?>
         <br>
     <?php endforeach; ?>
 
-    <!-- Кнопка для отправки формы -->
-    <button type="submit">Отправить</button>
+    <button type="submit">Закончить кринж-тайм</button>
 </form>
 
 </body>
