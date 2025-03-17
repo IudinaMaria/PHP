d66416937e20a6cd23d2a0bc3bfdec05ae6d059c
Maria Iudina <iudina.maria@usm.md>
2025-03-17 10:08:35 +0200

    Add files via upload

---
+++ lab_one_PHP/index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Привет, мир!<br>";
+
+echo "Hello, World with echo!<br>";
+print "Hello, World with print!<br>";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+// Вывод с конкатенацией
+echo "Прошло " . $days . " дней. " . $message . "<br>";
+
+// Вывод с двойными кавычками
+echo "Прошло $days дней. $message<br>";
+
+?>
